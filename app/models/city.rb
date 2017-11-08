15a8cafd09b8e291387ec1de7f83eb3c6f889c28
Rachel Salois <rachel.salois@gmail.com>
2017-11-08 16:34:22 -0500

    key change

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,6 @@
 class City < ApplicationRecord
   def get_forecast
-    key = Rails.application.secrets.weather_key
+    key = ENV['WEATHER_KEY']
     weather = RestClient.get("http://api.openweathermap.org/data/2.5/forecast?id=#{api_id}&appid=#{key}")
     JSON.parse(weather)
   end
