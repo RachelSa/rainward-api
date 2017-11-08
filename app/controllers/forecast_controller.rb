0c18893d78f04197e377cbb8ceaeb2f94c15ca60
Rachel Salois <rachel.salois@gmail.com>
2017-11-08 11:18:50 -0500

    add

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -2,23 +2,23 @@ class ForecastController < ApplicationController
 
   def suggestions
     cities = City.all
+     key = Rails.application.secrets.weather_key
 
     # for each city, get its forecast
     forecasts = cities.map do |city|
       weather = RestClient.get("http://api.openweathermap.org/data/2.5/forecast?id=#{city.api_id}&appid=#{key}")
       parsed_weather = JSON.parse(weather)
+      # for each city's forecasts, make a Forecast instance
       parsed_weather["list"].map do |w|
-        Forecast.create(current_temp:w["main"]["temp"], wind_speed:w["wind"]["speed"], cloud_coverage:w["clouds"]["all"], weather_code:w["weather"][0]['id'], city_id: city.id)
+        byebug
+        f = Forecast.create(current_temp:w["main"]["temp"], wind_speed:w["wind"]["speed"], cloud_coverage:w["clouds"]["all"], weather_code:w["weather"][0]['id'], city_id: city.id)
+        f.rate_forecast
+        byebug
       end
       # now rate each city based on its forecasts
-      cities.rate_forecast.save
+
+
     end
-
-    byebug
-    city.rate_forecast(parsed_weather)
-  end
-
-    render json: suggestions
   end
 
 
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,22 +1,5 @@
 class City < ApplicationRecord
 
-  def rate_forecast
-    if chilly?
-      rating += 1
-    end
-    if freezing?
-      rating += 1
-    end
-    if rainy?
-      rating += 2
-    end
-    if cloudy?
-      rating += 1
-    end
-    if windy?
-      rating += 1
-    end  
-  end
 
 
 
